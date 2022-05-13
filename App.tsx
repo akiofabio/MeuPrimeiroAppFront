a53a3a15e9a791d0845a565826efe4eb55cbf2b3
akiofabio <67397897+akiofabio@users.noreply.github.com>
2022-05-13 13:39:51 -0300

    up

--- App.tsx
+++ App.tsx
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, TextInput, View } from 'react-native';
+import { useState } from 'react'
 export default function App() {
+  const [text,setText] = useState(" ");
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <Text>Funcionou</Text>
+      <Text>Teste</Text>
+      <TextInput style={styles.input} onChangeText={(value)=>console.log(value)}></TextInput>
+      <TextInput style={styles.input} onChangeText={(value)=>setText(value)}></TextInput>
+      <Text>{text}</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -18,4 +22,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input: {
+    height: 60,
+    width: 180,
+    borderBottomWidth: 2
+  }
 });
